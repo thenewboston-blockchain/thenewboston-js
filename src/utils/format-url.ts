30ee8d70e792dadb6ae14f24d18e24613967d605
Carter Snook <cartersnook04@gmail.com>
2020-11-11 00:45:09 -0600

    chore(jsdoc): format url

--- src/utils/format-url.ts
+++ src/utils/format-url.ts
@@ -1,3 +1,7 @@
+/**
+ * Normalizes a url for requests to be made with.
+ * @param url the url string that must include the protocol (https, http, ect.) of the requests
+ */
 export function formatUrl(url: string) {
   return new URL(url).origin;
 }
