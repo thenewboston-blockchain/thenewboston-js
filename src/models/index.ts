dfda08ce7168250df03341d72968ab8a14ec98ec
Carter Snook <cartersnook04@gmail.com>
2020-11-11 00:47:04 -0600

    chore: export `Transaction`

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,3 +1,4 @@
 export type { Account } from "./account";
 export type { ServerNodeOptions } from "./server-node-options";
 export type { PaginationOptions } from "./pagination-options";
+export type { Transaction } from "./transaction";
