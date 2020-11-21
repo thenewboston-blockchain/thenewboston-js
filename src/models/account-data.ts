b1c0869e56f607ea9a3b04c9e3f88538c7bd30cd
Carter Snook <cartersnook04@gmail.com>
2020-11-20 21:37:33 -0600

    chore(jsdoc): `AccountData`

--- src/models/account-data.ts
+++ src/models/account-data.ts
@@ -1,3 +1,4 @@
+/** The interface for accounts that is mainly used internally for creating `Account` classes from existing data. */
 export interface AccountData {
   publicKey: Uint8Array;
   signingKey: Uint8Array;
