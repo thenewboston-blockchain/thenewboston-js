6247132c43018709f175e4ac79807131713ddf64
Carter Snook <cartersnook04@gmail.com>
2020-11-20 21:35:10 -0600

    chore(jsdoc): `BlockMessage`

--- src/models/block-message.ts
+++ src/models/block-message.ts
@@ -1,5 +1,10 @@
 import type { BlockData } from "./block-data";
 
+/**
+ * The interface for a block message that will be sent with requests.
+ *
+ * Note: this also includes all of the properties of a `BlockData` object.
+ */
 export interface BlockMessage extends BlockData {
   signature: string;
 }
