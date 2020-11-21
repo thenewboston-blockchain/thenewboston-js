98a02b9039b728344ee1b6973a810248fa69983e
Carter Snook <cartersnook04@gmail.com>
2020-11-20 21:36:24 -0600

    chore(jsdoc): `BlockData`

--- src/models/block-data.ts
+++ src/models/block-data.ts
@@ -1,5 +1,6 @@
 import type { Transaction } from "./transaction";
 
+/** The interface for the data that will be included within requests for transactions. */
 export interface BlockData {
   account_number: string;
   message: {
