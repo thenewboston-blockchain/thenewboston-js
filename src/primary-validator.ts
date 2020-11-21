268b4edb88534e13f6b8753dceba5873fb9a53ac
Carter Snook <cartersnook04@gmail.com>
2020-11-21 08:25:42 -0600

    chore(jsdoc): `PrimaryValidator`

--- src/primary-validator.ts
+++ src/primary-validator.ts
@@ -1,5 +1,6 @@
 import { Validator } from "./validator";
 
+/** Used for connecting with and using primary validator server nodes. */
 export class PrimaryValidator extends Validator {
   // TODO: POST /bank_blocks
   // TODO: POST /connection_requests
