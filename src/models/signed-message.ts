e8f82512c58247740b04e28ea6db67dbf478896d
Carter Snook <cartersnook04@gmail.com>
2020-11-20 21:53:13 -0600

    chore(jsdoc): `SignedMessage`

--- src/models/signed-message.ts
+++ src/models/signed-message.ts
@@ -1,5 +1,6 @@
 import type { SignedData } from "./signed-data";
 
+/** The interface for user authenticated messages. */
 export interface SignedMessage<T> extends SignedData<T> {
   node_identifier: string;
 }
