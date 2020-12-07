560ddaa6729f7d6fc24ac36fcaf9ebd0cd1280f0
mrbusysky <mrskymn@icloud.com>
2020-12-06 17:48:30 -0800

    Added webpack
    Webpack with generate a file that can be used in browser.

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
+  optimization: {
+    minimize: true
+  },
+  output: {
+    filename: 'tnb.min.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
