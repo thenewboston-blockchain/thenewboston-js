36759fd06ef0adef5a1dd3af8574d6006fdba332
Carter Snook <cartersnook04@gmail.com>
2020-12-07 16:50:37 -0600

    feat: only transpile cjs for es2015

--- rollup.config.js
+++ rollup.config.js
@@ -1,36 +1,64 @@
 import { getBabelOutputPlugin } from "@rollup/plugin-babel";
 import typescript from "rollup-plugin-typescript2";
 
+const input = "src/index.ts";
 const bundled = "dist/index";
-
-export default {
-  input: "src/index.ts",
-  output: [
-    {
-      file: `${bundled}.js`,
-      format: "cjs",
-    },
-    {
-      file: `${bundled}.esm.js`,
-      format: "esm",
-    },
-  ],
-  plugins: [
-    typescript(),
-    getBabelOutputPlugin({
-      presets: [
-        [
-          "@babel/preset-env",
-          {
-            targets: {
-              ie: "11",
-            },
-            useBuiltIns: "usage",
-            corejs: 3,
-          },
-        ],
-      ],
-    }),
-  ],
-  external: ["axios", "tweetnacl"],
+const output = {
+  cjs: `${bundled}.js`,
+  esm: `${bundled}.esm.js`,
 };
+
+const defaultPlugins = [typescript()];
+function generateBabelPluginWithTargets(targets) {
+  return getBabelOutputPlugin({
+    presets: [
+      [
+        "@babel/preset-env",
+        {
+          targets,
+          useBuiltIns: "usage",
+          corejs: 3,
+        },
+      ],
+    ],
+  });
+}
+const external = ["axios", "tweetnacl"];
+
+export default [
+  {
+    input,
+    output: [
+      {
+        file: output.cjs,
+        format: "cjs",
+      },
+    ],
+    plugins: [
+      ...defaultPlugins,
+      generateBabelPluginWithTargets({
+        ie: "11",
+      }),
+    ],
+    external,
+  },
+  {
+    input,
+    output: [
+      {
+        file: output.esm,
+        format: "esm",
+      },
+    ],
+    plugins: [
+      ...defaultPlugins,
+      generateBabelPluginWithTargets({
+        firefox: "54",
+        chrome: "51",
+        safari: "10",
+        opera: "38",
+      }),
+    ],
+    external,
+  },
+];
