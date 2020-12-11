90d59d3c71154587ee9c1c15923882abd96282ae
Nishp77 <nishp77@gmail.com>
2020-12-10 20:06:27 -0600

    update to use account signature!

--- src/bank.ts
+++ src/bank.ts
@@ -57,7 +57,7 @@ export class Bank extends ServerNode {
   async addBlocks(balanceLock: string, transactions: Transaction[], account: Account) {
     return await this.postData(
       "/blocks",
-      { account, balanceLock, transactions }
+      account.createSignedData({ balanceLock, transactions })
     )
   }
 
