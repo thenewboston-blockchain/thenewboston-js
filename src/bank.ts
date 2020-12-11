b47b5da74b40b50933fae98917bb0cc1b39fa996
Nishp77 <nishp77@gmail.com>
2020-12-10 20:14:22 -0600

    Fixed it

--- src/bank.ts
+++ src/bank.ts
@@ -57,7 +57,7 @@ export class Bank extends ServerNode {
   async addBlocks(balanceLock: string, transactions: Transaction[], account: Account) {
     return await this.postData(
       "/blocks",
-      account.createSignedData({ balanceLock, transactions })
+      account.createBlockMessage(balanceLock,transactions)
     )
   }
 
