8e58f8038e3c5fd5a737667630264d2558db5d9d
Abhay <67417765+AbhayAysola@users.noreply.github.com>
2021-03-05 17:32:59 +0530

    test: PaymentHandler Tests (#70)

---
+++ tests/paymentHandler.test.js
@@ -0,0 +1,27 @@
+const tnb = require("../");
+
+describe("PaymentHandler", () => {
+  const bank = new tnb.Bank("http://13.57.215.62");
+  const pv = new tnb.PrimaryValidator("http://54.241.124.162/");
+  const paymentHandler = new tnb.PaymentHandler({ bankUrl: bank.url });
+
+  function assertPaymentHandlerBasics(paymentHandler) {
+    expect(paymentHandler.bank.url).toBe(bank.url);
+    expect(paymentHandler.primaryValidator.url).toBe(pv.url);
+  }
+
+  it("constructor()", async () => {
+    await paymentHandler.init();
+    assertPaymentHandlerBasics(paymentHandler);
+  });
+
+  it("createTransaction(transferDetails)", async () => {
+    paymentHandler.init();
+    const sender = new tnb.Account();
+    const recipient = new tnb.Account();
+    const transaction = await paymentHandler.createTransaction({ sender, recipient, amount: 1 });
+    expect(transaction.sender).toBe(sender);
+    expect(transaction.balanceLock).toBe((await pv.getAccountBalanceLock(sender.accountNumberHex)).balance_lock);
+    expect(transaction.transactions[0]).toStrictEqual({ amount: 1, recipient: recipient.accountNumberHex });
+  });
+});
